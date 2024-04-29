b716773b39b6f63f9113736003642cd3fba81ac7
Kai Xu <xuk@ibm.com>
2024-04-28 22:29:31 -0400

    chore: update prompts
    Signed-off-by: Kai Xu <xuk@ibm.com>

--- src/main/chat.cljs
+++ src/main/chat.cljs
@@ -4,8 +4,8 @@
 (def format-instruction-markdown
   "Please assist by reading and responding in Markdown syntax used by Logseq's blocks, with the following additional notes:
 * Use `*` for lists instead of `-`.
-* Do NOT include headings.
-* Do NOT use nested lists and sub-items by avoding itemizing top-level texts.")
+* Avoid using headings.
+* Avoid using nested lists and sub-items.")
 
 (def format-instruction-org
   "Please assist by reading and responding in Org mode syntax used by Logseq's blocks, with the following additional notes:
@@ -14,9 +14,9 @@
   *bold*, =verbatim=, /italic/, +strikethrough+, _underline_, ~code~, [[protocal://some.domain][some label]]
   #+END_SRC
 - Note that bold uses single ~*~ to quote,, i.e. ~*bold*~ instead of ~**bold**~.
-- Do NOT include headings.
-- Do NOT use nested lists and sub-items by avoding itemizing top-level texts.
-- Do NOT quote the entire response in a greater block.")
+- Avoid using headings.
+- Avoid using nested lists and sub-items.
+- Avoid quoting the entire response in a greater block.")
 
 (defn augment-system-message [system-message format]
   (let [format-instruction (cond
